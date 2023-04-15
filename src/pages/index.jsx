d673e4a45aa6670e2d78448a700d5ca02e71ff8e
hahayz77 <danilo__danilo@hotmail.com>
2023-04-15 20:44:18 -0300

    nextjsSample

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className='text-3xl text-red-500'>Hello World!</h1>
+    </main>
+  )
+}

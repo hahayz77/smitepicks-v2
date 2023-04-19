b8faf557756f9b308033736099040c44f7008900
hahayz77 <danilo__danilo@hotmail.com>
2023-04-18 21:10:37 -0300

    link correction

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     <section id="home">
         <h1 className='title'>Smite Draft</h1>
         <h2 className="text-center text-cyan-900">The Gods Will Draft!</h2>
-        <Link href={'/draft'}><button className="btn w-full px-0 mx-0 py-2">New Draft</button></Link>
+        <Link href={'/draft/1'}><button className="btn w-full px-0 mx-0 py-2">New Draft</button></Link>
         <h3 className="text-center text-white">Or join with a code...</h3>
         <div className="join py-4">
             <input className="w-full px-0 mx-0 py-2 text-center text-xl text-white tracking-[1rem] uppercase" type="text" maxLength={6}/>
